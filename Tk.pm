65ee2bc0ad0c7822e1630be3582d7c2ea7524acf
Slaven Rezić <slaven@rezic.de>
2003-06-07 19:10:20 +0000

    Import of SREZIC/Patch-SREZIC-Tk-800_024.011 from CPAN.
    gitpan-cpan-distribution: Patch-SREZIC-Tk
    gitpan-cpan-version:      800_024.011
    gitpan-cpan-path:         SREZIC/Patch-SREZIC-Tk-800_024.011.tar.gz
    gitpan-cpan-author:       SREZIC
    gitpan-cpan-maturity:     released

---
+++ Tk.pm
@@ -0,0 +1,20 @@
+# -*- perl -*-
+
+#
+# $Id: Tk.pm,v 1.39 2003/06/07 19:06:41 eserte Exp $
+# Author: Slaven Rezic
+#
+# Mail: slaven@rezic.de
+# WWW:  http://www.rezic.de/eserte/
+#
+
+package Patch::SREZIC::Tk;
+use vars qw($VERSION);
+
+$VERSION = "800_024.011";
+
+warn "This file does nothing --- please patch the \"real\" Tk distribution";
+
+1;
+
+__END__
